6c749905bea52d6f1ddbfe0f62e6d7702a646701
Leandro <leandro.almeida@uvvnet.com.br>
2023-04-03 12:08:11 -0300

    definindo as propriedades da interface

--- LeoShopping.ProductAPI/Repository/Interfaces/IProductRepository.cs
+++ LeoShopping.ProductAPI/Repository/Interfaces/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace LeoShopping.ProductAPI.Repository.Interfaces
+﻿using LeoShopping.ProductAPI.Data.Dtos;
+
+namespace LeoShopping.ProductAPI.Repository.Interfaces
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDTO>> FindAll();
+        Task<ProductDTO> FindById(long id);
+        Task<ProductDTO> Create(ProductDTO dto);
+        Task<ProductDTO> Update(ProductDTO dto);
+        Task<bool> DeleteById(long id);
     }
-}
+}   
