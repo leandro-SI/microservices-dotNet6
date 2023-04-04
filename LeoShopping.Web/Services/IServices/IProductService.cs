20d9310f9cf970541f12b9940f6b645838558acc
Leandro <leandro.almeida@uvvnet.com.br>
2023-04-04 16:10:42 -0300

    operações da interface IProductService

---
+++ LeoShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using LeoShopping.Web.Models;
+
+namespace LeoShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
