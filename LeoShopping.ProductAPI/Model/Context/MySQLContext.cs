20f4b2abd11d549fc24eaa8376e1ca2ce83631f1
Leandro <leandro.almeida@uvvnet.com.br>
2023-04-04 00:03:08 -0300

    config Seed Data no Microservoço de Produtos

--- LeoShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ LeoShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -16,5 +16,43 @@ namespace LeoShopping.ProductAPI.Model.Context
 
         public DbSet<Product> Products { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Camiseta Spacex",
+                Price = 69.9m,
+                Description = "Mussum Ipsum, cacilds vidis litro abertis.",
+                CategoryName = "T-shirt",
+                ImageURL = "https://github.com/leandro-SI/microservices-dotNet6/blob/main/LeoShopping.ProductAPI/Imagens/6_spacex.jpg?raw=true"
+
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Camiseta GNU Linux",
+                Price = 70.5m,
+                Description = "Mussum Ipsum, cacilds vidis litro abertis.",
+                CategoryName = "T-shirt",
+                ImageURL = "https://github.com/leandro-SI/microservices-dotNet6/blob/main/LeoShopping.ProductAPI/Imagens/12_gnu_linux.jpg?raw=true"
+
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Camiseta Coffee",
+                Price = 39.5m,
+                Description = "Mussum Ipsum, cacilds vidis litro abertis.",
+                CategoryName = "T-shirt",
+                ImageURL = "https://github.com/leandro-SI/microservices-dotNet6/blob/main/LeoShopping.ProductAPI/Imagens/7_coffee.jpg?raw=true"
+
+            });
+
+            //base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
