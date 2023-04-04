91cd269058383b6cfa6ce2cc2f2fd0755bd4b431
Leandro <leandro.almeida@uvvnet.com.br>
2023-04-04 00:09:17 -0300

    executando o Seed de Produtos

--- LeoShopping.ProductAPI/Migrations/20230404030818_SeedProductDataTable.Designer.cs
+++ LeoShopping.ProductAPI/Migrations/20230404030818_SeedProductDataTable.Designer.cs
@@ -2,6 +2,7 @@
 using LeoShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeoShopping.ProductAPI.Migrations
 {
     [DbContext(typeof(MySQLContext))]
-    partial class MySQLContextModelSnapshot : ModelSnapshot
+    [Migration("20230404030818_SeedProductDataTable")]
+    partial class SeedProductDataTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LeoShopping.ProductAPI/Migrations/20230404030818_SeedProductDataTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeoShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 3L, "T-shirt", "Mussum Ipsum, cacilds vidis litro abertis.", "https://github.com/leandro-SI/microservices-dotNet6/blob/main/LeoShopping.ProductAPI/Imagens/6_spacex.jpg?raw=true", "Camiseta Spacex", 69.9m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 4L, "T-shirt", "Mussum Ipsum, cacilds vidis litro abertis.", "https://github.com/leandro-SI/microservices-dotNet6/blob/main/LeoShopping.ProductAPI/Imagens/12_gnu_linux.jpg?raw=true", "Camiseta GNU Linux", 70.5m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 5L, "T-shirt", "Mussum Ipsum, cacilds vidis litro abertis.", "https://github.com/leandro-SI/microservices-dotNet6/blob/main/LeoShopping.ProductAPI/Imagens/7_coffee.jpg?raw=true", "Camiseta Coffee", 39.5m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+        }
+    }
+}
