81d9b28dd8a29c4e355511ec5b3f1429299fa526
Leandro <leandro.almeida@uvvnet.com.br>
2023-04-11 16:34:49 -0300

    implementando Controller de Produtos

---
+++ LeoShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using LeoShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeoShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+
+            var products = await _productService.FindAllProducts();
+
+            return View(products);
+        }
+    }
+}
