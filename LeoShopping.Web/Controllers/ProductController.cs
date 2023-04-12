ed60985fe31f45b316e0005fdd6588859dc0799c
Leandro <leandro.almeida@uvvnet.com.br>
2023-04-12 14:43:26 -0300

    delete Produto

--- LeoShopping.Web/Controllers/ProductController.cs
+++ LeoShopping.Web/Controllers/ProductController.cs
@@ -76,5 +76,34 @@ namespace LeoShopping.Web.Controllers
 
         }
 
+        public async Task<IActionResult> ProductDelete(long id)
+        {
+            var product = await _productService.FindProductById(id);
+
+            if (product != null)
+            {
+                return View(product);
+            }
+
+            return NotFound();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+
+            var response = await _productService.DeleteProductById(model.Id);
+
+            if (response)
+            {
+                return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(model);
+
+        }
+
+
     }
 }
