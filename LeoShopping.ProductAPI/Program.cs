6064b47daa3b49d02e667a29dda5657874ba9be7
Leandro <leandro.almeida@uvvnet.com.br>
2023-04-17 11:56:50 -0300

    add Autenticação ao Swagger

--- LeoShopping.ProductAPI/Program.cs
+++ LeoShopping.ProductAPI/Program.cs
@@ -47,11 +47,6 @@ builder.Services.AddAuthorization(options =>
 
 });
 
-
-
-
-
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -70,6 +65,30 @@ builder.Services.AddSwaggerGen(c =>
     
     });
     c.EnableAnnotations();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"Enter 'Bearer' [space] and your token!",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header
+        },
+        new List<string>()
+        }
+    }) ;
 });
 
 var app = builder.Build();
