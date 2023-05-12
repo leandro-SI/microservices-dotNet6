c3273e202c11ffec7219770415c90e01f33ad05d
Leandro <leandro.almeida@uvvnet.com.br>
2023-05-12 11:45:00 -0300

    implementando cadastro de novos usuários

--- LeoShopping.ProductAPI/Program.cs
+++ LeoShopping.ProductAPI/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddControllers();
 
 
 builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer(options =>
+    .AddJwtBearer("Bearer", options =>
     {
         options.Authority = "https://localhost:4435/";
         options.TokenValidationParameters = new TokenValidationParameters
