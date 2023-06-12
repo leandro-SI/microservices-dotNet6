a9cee8d9082992064d6d659adb69a8a12047e98e
Leandro <leandro.almeida@uvvnet.com.br>
2023-06-12 15:56:29 -0300

    Centralizando as requisições com API Gateway utilizando Ocelot

---
+++ LeoShopping.APIGateway/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+
+    });
+
+builder.Services.AddOcelot();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseOcelot();
+app.Run();
